bb19f3f962f5a2e0116d61fdd542e520482d7062
Akhmad Fauzan <akhmadfauzan1604@gmail.com>
2025-11-18 12:13:41 +0800

    feat(radiologi): implement index method to retrieve and paginate radiology lab data for doctors

--- app/Http/Controllers/Dokter/RadiologiController.php
+++ app/Http/Controllers/Dokter/RadiologiController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\Dokter;
 
 use App\Http\Controllers\Controller;
+use App\Models\Radiologi;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class RadiologiController extends Controller
 {
     public function index(): View
     {
-        return view('dokter.radiologi.index');
+        $dokter = Auth::user()->dokter;
+        
+        $labs = Radiologi::with(['pasien.user', 'dokter.user'])
+            ->where('dokter_id', $dokter->id)
+            ->latest()
+            ->paginate(15);
+
+        return view('dokter.radiologi.index', compact('labs'));
     }
 }
